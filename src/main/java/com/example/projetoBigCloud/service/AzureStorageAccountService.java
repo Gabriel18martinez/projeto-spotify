d16f4484ab071bd2d137bc38885dd18b18c4939d
Gabriel18martinez <1805martinez@gmail.com>
2024-06-19 01:11:25 -0300

    adicionando o Blob

--- src/main/java/com/example/projetoBigCloud/service/AzureStorageAccountService.java
+++ src/main/java/com/example/projetoBigCloud/service/AzureStorageAccountService.java
@@ -16,7 +16,21 @@ import java.util.UUID;
 public class AzureStorageAccountService {
     private final String connectionString = "DefaultEndpointsProtocol=https;AccountName=bigcloudspotify;AccountKey=tGqmzzRCdnbs3l0T1EOzxPdzhRd6RwcVtAhuOhbE79vdAxOvUD37DCrhLkfqyJ+I3Yei+FqfAvPN+AStwJTS8w==;EndpointSuffix=core.windows.net";
     public String uploadFileToAzure(String imageBase64) throws IOException {
+        BlobContainerClient client = new BlobContainerClientBuilder()
+                .connectionString(this.connectionString)
+                .containerName("images")
+                .buildClient();
 
+        String fileName = UUID.randomUUID() + ".png";
+        BlobClient blob = client.getBlobClient(fileName);
+
+        byte[] image = Base64.getDecoder().decode(imageBase64);
+
+        BinaryData binDataImage = BinaryData.fromBytes(image);
+
+        blob.upload(binDataImage, true);
+
+        return "https://cargalleryimagestorage.blob.core.windows.net/images/" + fileName;
 
     }
 }
